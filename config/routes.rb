5cd56fdeb965e16e55de755db21dda7c7564a3db
Michael Moffitt <mikeymoff91@gmail.com>
2019-04-11 14:18:57 -0600

    navbar styling add card button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
-<<<<<<< HEAD
-
-=======
->>>>>>> Added style for nav bar and to card
     root 'api/decks#index'
 
   namespace :api do
