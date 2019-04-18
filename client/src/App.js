de92028d4ab22ba76522e37d21e260dad0b1982f
Michael Moffitt <mikeymoff91@gmail.com>
2019-04-17 19:13:09 -0600

    added focus button and deck

--- client/src/App.js
+++ client/src/App.js
@@ -21,8 +21,6 @@ import DeckHTML from './components/DeckHTML';
 import Decks from './components/Decks';
 import ShowDeck from './components/ShowDeck';
 
-import DeckHTML from './components/DeckHTML';
-
 const App = () => (
 
 <div>
