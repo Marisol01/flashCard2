fe7c40206fadc498fcdcba222743f278caf12836
Marisoln Araujo <u167272@gmail.com>
2019-04-21 14:09:17 -0600

    added search bar

--- client/src/components/SideDrawer/SideDrawer.js
+++ client/src/components/SideDrawer/SideDrawer.js
@@ -10,6 +10,17 @@ const sideDrawer = props => {
  return (
    <nav className={drawerClasses}>
      <ul>
+      
+
+      <div class="body">
+      <div class="search-box">
+        <input class="search-txt" type="text" name="" placeholder="Search"></input>
+        <img src="iconfinder_11_Search_106236.png" class="search-btn"> 
+        </img>
+      </div>
+      </div>
+          
+
        <li><Link to={{
                 pathname:`/card`, //api flow for this?
                 state: {
